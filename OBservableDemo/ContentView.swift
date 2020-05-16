19a2b4af4e838138a36c5ab932719f49558b50a5
junhyuk <dejavukk1@gmail.com>
2020-05-16 09:54:23 +0900

    Initial Commit

---
+++ OBservableDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OBservableDemo
+//
+//  Created by junhyuk on 2020/05/16.
+//  Copyright © 2020 junhyuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
